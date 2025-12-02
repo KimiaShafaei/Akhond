bc93c57bd18702964a29379440409cea645f218f
KimiaShafaei <169140834+KimiaShafaei@users.noreply.github.com>
2025-12-02 12:46:22 -0800

    Added player movements

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D player_body;
+
+    private Vector2 move_direction;
+    
+    [SerializeField]
+    private InputActionReference move_action;
+    [SerializeField]
+    private float move_speed = 5f;
+
+    [SerializeField]
+    private InputActionReference jump_action;
+    [SerializeField]
+    private float jump_force = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player_body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move_direction = move_action.action.ReadValue<Vector2>();
+
+        if (jump_action.action.WasPressedThisFrame())
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        player_body.linearVelocity = new Vector2(move_direction.x * move_speed, player_body.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        player_body.linearVelocity = new Vector2(player_body.linearVelocity.x, jump_force);
+    }
+}
