21916dadeb17281a53b92af265ca9813bdee11ce
KimiaShafaei <169140834+KimiaShafaei@users.noreply.github.com>
2025-12-04 11:53:39 -0800

    Add Fire Station mechanics

---
+++ Assets/Scripts/Fire_Station/FireStation.cs
@@ -0,0 +1,46 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FireStation : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private float toggle_time = 2f;
+    private bool is_on = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(ToggleRoutine());
+    }
+
+    private System.Collections.IEnumerator ToggleRoutine()
+    {
+        while (true)
+        {
+            ToggleState();
+            yield return new WaitForSeconds(toggle_time);
+        }
+    }
+
+    void ToggleState()
+    {
+        is_on = !is_on;
+        animator.SetBool("IsOn", is_on);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!is_on)
+        {
+            return;
+        }
+
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
